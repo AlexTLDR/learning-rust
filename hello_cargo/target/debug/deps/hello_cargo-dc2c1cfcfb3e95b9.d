7e2b18b88cc3a97639a62b2cb716764853575b96
AlexTLDR <alex.badragan@protonmail.com>
2023-08-18 11:13:41 +0200

    hello world with or without cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-dc2c1cfcfb3e95b9.d
@@ -0,0 +1,5 @@
+/Users/alex.badragan/git/learning-rust/hello_cargo/target/debug/deps/hello_cargo-dc2c1cfcfb3e95b9: src/main.rs
+
+/Users/alex.badragan/git/learning-rust/hello_cargo/target/debug/deps/hello_cargo-dc2c1cfcfb3e95b9.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/alex.badragan/git/learning-rust/hello_cargo/target/debug/hello_cargo: /Users/alex.badragan/git/learning-rust/hello_cargo/src/main.rs
